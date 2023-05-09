d6f99dd5b69889cc31a0b4cb7bd68bec34808708
ufazaa55 <ufazaa55@gmail.com>
2023-05-09 13:47:31 +0700

    building function

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-console.log("HELLO WORLD!");
-console.log("Hi");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const zod_1 = require("zod");
+const app = (0, express_1.default)();
+const port = 3000;
+const schema = zod_1.z.object({
+    name: zod_1.z.string(),
+    lastName: zod_1.z.string(),
+    age: zod_1.z.number().refine((n) => n === 30, "age must be equal 30"),
+});
+app.use(express_1.default.json());
+app.get("/", (req, res) => {
+    res.send("Hello World Hi!");
+});
+app.post("/", (req, res) => {
+    const input = schema.safeParse(req.body);
+    if (input.success) {
+        res.send(`Hello ${input.data.name} ${input.data.lastName} ${input.data.age} !`);
+    }
+    else {
+        res.json(input.error);
+    }
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
--- index.ts
+++ index.ts
@@ -1,2 +1,36 @@
-const t ="hello world";
-console.log(t);
+import express from "express";
+import { z } from "zod";
+const app = express();
+const port = 3000;
+
+const schema = z.object({
+    name: z.string(),
+    lastName: z.string(),
+    age: z.number().refine((n) => n === 30 , "age must be equal 30"),
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    const rt:z.infer <typeof schema> = {
+        name: "ufa",
+        lastName: "manit",
+        age: 30,
+    };
+    res.send("Hello World Hi!");
+});
+
+app.post("/", (req, res) => {
+    const input = schema.safeParse(req.body);
+    if (input.success){
+        res.send(`Hello ${input.data.name} ${input.data.lastName} ${input.data.age} !`);
+    }
+    else {
+        res.json(input.error);
+      }
+   
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
